37bc93355cf6c5dd8ce8a9b99b40f74cb7fe1f89
dmitri9149 <dmitri.as.name@gmail.com>
2020-09-27 13:17:03 +0300

    convert all in project

--- IonideProject/Program.fs
+++ IonideProject/Program.fs
@@ -2,7 +2,25 @@
 
 open System
 
+module PigLatin = 
+// converts a word to PigLatin representation
+// from https://docs.microsoft.com/en-us/dotnet/fsharp/get-started/get-started-vscode
+
+    let toPigLatin (word: string) =
+        let isVowel (c: char) =
+            match c with
+            | 'a' | 'e' | 'i' |'o' |'u'
+            | 'A' | 'E' | 'I' | 'O' | 'U' -> true
+            |_ -> false
+        
+        if isVowel word.[0] then
+            word + "yay"
+        else
+            word.[1..] + string(word.[0]) + "ay"
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    for name in argv do
+        let newName = PigLatin.toPigLatin name
+        printfn "%s in Pig Latin is: %s" name newName
     0 // return an integer exit code
